67141237817104877adf72cd44f7b8db8251f173
AlphaCygni <alpha.cygni44@gmail.com>
2015-09-09 20:40:29 -0400

    First commit

---
+++ SumOfPrimes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumFirstThousandPrimes());
+            Console.ReadLine();
+        }
+
+        static long SumFirstThousandPrimes()
+        {
+            var primeCtr = 0;
+            var number = 1;
+            var sum = 0;
+            bool isPrime;
+
+            do
+            {
+                if (number == 1)
+                {
+                    isPrime = false;
+                }
+                else
+                {
+                    isPrime = true;
+                    
+                    for (var divisor = 1; divisor <= number / 2; divisor++)
+                    {
+                        if (number % divisor == 0 && divisor != 1)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isPrime)
+                {
+                    sum += number;
+                    primeCtr++;
+                }
+
+                number++;
+            } while (primeCtr < 1000);
+
+            return sum;
+        }
+    }
+}
